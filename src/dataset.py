ee5defc44c856f2f2dc9b74e4eb2e98fcc2e582e
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-05-18 09:37:47 +0900

    [change] read npy

--- src/dataset.py
+++ src/dataset.py
@@ -40,16 +40,19 @@ class WaveformDataset(torchdata.Dataset):
     def __init__(self,
                  df: pd.DataFrame,
                  datadir: Path,
-                 img_size=224,
                  waveform_transforms=None,
                  period=20,
                  validation=False):
         self.df = df
         self.datadir = datadir
-        self.img_size = img_size
         self.waveform_transforms = waveform_transforms
         self.period = period
         self.validation = validation
+
+    def _normalize(self, arr):
+        max_arr = np.max(arr)
+        min_arr = np.min(arr)
+        return (arr - min_arr) / (max_arr - min_arr)
 
     def __len__(self):
         return len(self.df)
@@ -59,29 +62,31 @@ class WaveformDataset(torchdata.Dataset):
         wav_name = sample["filename"]
         ebird_code = sample["primary_label"]
 
-        y, sr = sf.read(self.datadir / ebird_code / wav_name)
+        y = np.read(self.datadir / ebird_code / wav_name / '.npy')
+        y = self._normalize(y) 
+
         if self.waveform_transforms:
             y = self.waveform_transforms(y)
 
-        len_y = len(y)
+        len_y = y.shape[1]
         effective_length = sr * self.period
         if len_y < effective_length:
-            new_y = np.zeros(effective_length, dtype=y.dtype)
+            new_y = np.zeros((y.shape[0], effective_length), 
+                    dtype=y.dtype)
             if not self.validation:
                 start = np.random.randint(effective_length - len_y)
             else:
                 start = 0
-            new_y[start:start + len_y] = y
-            y = new_y.astype(np.float32)
+            new_y[:, start:start + len_y] = y
+            y = new_y
         elif len_y > effective_length:
             if not self.validation:
                 start = np.random.randint(len_y - effective_length)
             else:
                 start = 0
-            y = y[start:start + effective_length].astype(np.float32)
-        else:
-            y = y.astype(np.float32)
+            y = y[:, start:start + effective_length]
 
+        y = y.astype(np.float32)
         y = np.nan_to_num(y)
 
         labels = np.zeros(len(CFG.target_columns), dtype=float)
